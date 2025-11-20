31f38c28076f13aa05e09acf9f61dfc5c00654bd
aiman qureshi <qureshiaiman916@gmail.com>
2025-11-20 17:27:09 +0530

    some changes

--- src/app/shared/component/students/students.component.ts
+++ src/app/shared/component/students/students.component.ts
@@ -11,64 +11,69 @@ import { Istudent } from '../../models/student';
   styleUrls: ['./students.component.scss']
 })
 export class StudentsComponent implements OnInit {
-  @ViewChild('stdForm') stdForm!:NgForm
-  isInEditMode:boolean=false;
-  Edit_Id!:string
-students:Array<Istudents>=[
-  {
-fname:"Aiman",
-lname:"Q",
-email:"aimanq@gmail.com",
-contact:1234556778,
-age:23,
-stdId:"123"
-  }
-]
-  constructor(private _uuid:UuidService,
-    private _snackBar:SnackbarService
+  @ViewChild('stdForm') stdForm!: NgForm
+  isInEditMode: boolean = false;
+  Edit_Id!: string
+  students: Array<Istudents> = [
+    {
+      fname: "Aiman",
+      lname: "Q",
+      email: "aimanq@gmail.com",
+      contact: 1234556778,
+      age: 23,
+      stdId: "123"
+    }
+  ]
+  constructor(private _uuid: UuidService,
+    private _snackBar: SnackbarService
   ) { }
 
   ngOnInit(): void {
   }
-onStdAdd(){
-  if(this.stdForm.valid){
-    let stdObj={
-      ...this.stdForm.value,
-           stdId:this._uuid.Uuid()
+  onStdAdd() {
+    if (this.stdForm.valid) {
+      let stdObj = {
+        ...this.stdForm.value,
+        contact: +this.stdForm.controls['contact'].value,
+        age: +this.stdForm.controls['age'].value,
+        stdId: this._uuid.Uuid()
+      }
+      this.students.unshift(stdObj);
+      this.stdForm.reset();
+      this._snackBar.openSnackBar('New student added successfully !')
+    } else {
+      alert('pls fill all input first !')
     }
-    this.students.unshift(stdObj);
-    this.stdForm.reset();
-     this._snackBar.openSnackBar('New student added successfully !')
-  }else{
-    alert('pls fill all input first !')
   }
-}
-onStdRemove(stdObj:Istudents){
-  let getConfirmation=confirm('Are you sure you want to remove this student ?')
-  if(getConfirmation){
-    let getIndex=this.students.findIndex(std=>std.stdId===stdObj.stdId)
-    this.students.splice(getIndex,1)
-  }
-}
-onStdEdit(std:Istudents){
-  this.isInEditMode=true;
-  this.Edit_Id=std.stdId;
-  localStorage.setItem("Edit_Id",this.Edit_Id);
-  this.stdForm.form.patchValue(std)
-}
-onStdUpdate(){
-  let Updated_Id=localStorage.getItem("Edit_Id")
-  if(this.stdForm.valid && Updated_Id){
-    let updatedObj={
-      ...this.stdForm.value,
-      stdId:Updated_Id
+  onStdRemove(stdObj: Istudents) {
+    let getConfirmation = confirm('Are you sure you want to remove this student ?')
+    if (getConfirmation) {
+      let getIndex = this.students.findIndex(std => std.stdId === stdObj.stdId)
+      this.students.splice(getIndex, 1)
     }
-    let getIndex=this.students.findIndex(std=>std.stdId===Updated_Id)
-    this.students[getIndex]=updatedObj;
-    this.stdForm.reset();
-    this.isInEditMode=false;
-    this.Edit_Id=''
-    this._snackBar.openSnackBar('This student updated successfully !')
   }
-}
+  onStdEdit(std: Istudents) {
+    this.isInEditMode = true;
+    this.Edit_Id = std.stdId;
+    localStorage.setItem("Edit_Id", this.Edit_Id);
+    this.stdForm.form.patchValue(std)
+  }
+  onStdUpdate() {
+    let Updated_Id = localStorage.getItem("Edit_Id")
+    if (this.stdForm.valid && Updated_Id) {
+      let updatedObj = {
+        ...this.stdForm.value,
+        contact: +this.stdForm.controls['contact'].value,
+        age: +this.stdForm.controls['age'].value,
+
+        stdId: Updated_Id
+      }
+      let getIndex = this.students.findIndex(std => std.stdId === Updated_Id)
+      this.students[getIndex] = updatedObj;
+      this.stdForm.reset();
+      this.isInEditMode = false;
+      this.Edit_Id = ''
+      this._snackBar.openSnackBar('This student updated successfully !')
+    }
+  }
 }
--- src/app/shared/component/todos/todos.component.ts
+++ src/app/shared/component/todos/todos.component.ts
@@ -10,67 +10,67 @@ import { SnackbarService } from '../../service/snackbar.service';
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
-  @ViewChild('todoForm') todoForm!:NgForm
-  isInEditMode:boolean=false;
-  Edit_Id!:string
-todos:Array<Itodo>=[
-  {
-    todoItem:"Java",
-    todoId:"123"
-  },
-  {
-    todoItem:"Saas",
-    todoId:"124"
-  }
-]
-  constructor(private _uuid:UuidService,
-    private _snackBar:SnackbarService
+  @ViewChild('todoForm') todoForm!: NgForm
+  isInEditMode: boolean = false;
+  Edit_Id!: string
+  todos: Array<Itodo> = [
+    {
+      todoItem: "Java",
+      todoId: "123"
+    },
+    {
+      todoItem: "Saas",
+      todoId: "124"
+    }
+  ]
+  constructor(private _uuid: UuidService,
+    private _snackBar: SnackbarService
   ) { }
 
   ngOnInit(): void {
   }
 
-  trackById(i:number,todoObj:Itodo){
-return todoObj.todoId
+  trackById(i: number, todoObj: Itodo) {
+   console.log(todoObj.todoId)
   }
 
-onTodoSubmit(){
-  if(this.todoForm.valid){
-  let todoObj={
-    ...this.todoForm.value,
-    todoId:this._uuid.Uuid()
+  onTodoSubmit() {
+    if (this.todoForm.valid) {
+      let todoObj = {
+        ...this.todoForm.value,
+        todoId: this._uuid.Uuid()
+      }
+      this.todos.unshift(todoObj);
+      this.todoForm.reset()
+      this._snackBar.openSnackBar('New todo added successfully !')
+    } else {
+      alert('Add todo first !')
+    }
   }
-  this.todos.unshift(todoObj);
-  this.todoForm.reset()
-  this._snackBar.openSnackBar('New todo added successfully !')
-}else{
-  alert('Add todo first !')
-}
-}
-onTodoRemove(list:Itodo){
-  let getConfirmation=confirm('Are you sure you want to remove this todo !')
-  if(getConfirmation){
-    let getindex=this.todos.findIndex(todo=>todo.todoId===list.todoId)
-this.todos.splice(getindex,1)
+  onTodoRemove(list: Itodo) {
+    let getConfirmation = confirm('Are you sure you want to remove this todo !')
+    if (getConfirmation) {
+      let getindex = this.todos.findIndex(todo => todo.todoId === list.todoId)
+      this.todos.splice(getindex, 1)
+    }
+    this._snackBar.openSnackBar('This todo removed successfully !')
   }
-  this._snackBar.openSnackBar('This todo removed successfully !')
-}
-onEditTodo(list:Itodo){
-  this.isInEditMode=true
- this.Edit_Id=list.todoId;
-localStorage.setItem("Edit_Id",this.Edit_Id)
-this.todoForm.form.patchValue(list)
-}
-onUpdateTodo(){
-  let Updated_Id=localStorage.getItem("Edit_Id")
-  if(this.todoForm.valid && Updated_Id){
-    let updatedObj={...this.todoForm.value,todoId:Updated_Id}
-    let getIndex=this.todos.findIndex(todo=>todo.todoId===Updated_Id)
-    this.todos[getIndex]=updatedObj
-    this.todoForm.reset()
-    this.isInEditMode=false;
-    this.Edit_Id=''
+  onEditTodo(list: Itodo) {
+    this.isInEditMode = true
+    this.Edit_Id = list.todoId;
+    localStorage.setItem("Edit_Id", this.Edit_Id)
+    this.todoForm.form.patchValue(list)
   }
-  this._snackBar.openSnackBar('This todo updated successfully ! ')
-}
+  onUpdateTodo() {
+    let Updated_Id = localStorage.getItem("Edit_Id")
+    if (this.todoForm.valid && Updated_Id) {
+      let updatedObj = { ...this.todoForm.value, todoId: Updated_Id }
+      let getIndex = this.todos.findIndex(todo => todo.todoId === Updated_Id)
+      this.todos[getIndex] = updatedObj
+      this.todoForm.reset()
+      this.isInEditMode = false;
+      this.Edit_Id = ''
+      this._snackBar.openSnackBar('This todo updated successfully ! ')
+    }
+  }
 }
